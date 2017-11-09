5f379130c7a9bc1187a39a9e57865a3789346944
root <root@coleen-vm-client.coleen-vm-client>
2017-11-09 22:19:25 +0000

    vlan.rb

--- lib/puppet/provider/cnos_vlan/vlan.rb
+++ lib/puppet/provider/cnos_vlan/vlan.rb
@@ -11,29 +11,39 @@ Puppet::Type.type(:cnos_vlan).provide :vlan do
  
  #mk_resource_methods
  conn = Connect.new('./config.yml')
+
  def self.instances
      conn = Connect.new('./config.yml')
      resp = Vlan.get_all_vlan(conn)
      return 'no vlans' if !resp
      resp.each do  |item|
-      puts item['vlan_id']
       provider_val = {:name => item['vlan_id'], :vlan_name => item['vlan_name'],:ensure => :present}
       provider_val[:admin_state] = item['admin_state']
       puts provider_val
       new(provider_val)
      end
  end
+ def self.prefetch(resources)
+     vlans = instances
+     resources.keys.each do |name|
+	     if provider = vlans.find { |vlan| vlan.name == name }
+		resources[name].provider = provider
+	     end
+     end
+ end
  
  def admin_state
-     conn = Connect.new('./config.yml')
-     resp = Vlan.get_vlan_prop(conn, resource[:vlan_id])
-     resp['admin_state']
+     #conn = Connect.new('./config.yml')
+     #resp = Vlan.get_vlan_prop(conn, resource[:vlan_id])
+     #resp['admin_state']
+     @property_hash[:admin_state]
  end 
  
  def vlan_name
-     conn = Connect.new('./config.yml')
-     resp = Vlan.get_vlan_prop(conn, resource[:vlan_id])
-     resp['vlan_name']
+     #conn = Connect.new('./config.yml')
+     #resp = Vlan.get_vlan_prop(conn, resource[:vlan_id])
+     #resp['vlan_name']
+     @property_hash[:vlan_name]
      
  end 
 
@@ -42,7 +52,7 @@ Puppet::Type.type(:cnos_vlan).provide :vlan do
      params = {'vlan_name' => resource[:vlan_name], 
                'admin_state' => resource[:admin_state]}
      resp = Vlan.update_vlan(conn, resource[:vlan_id], params)
-   #@property_hash[:admin_state] = value
+     @property_hash[:admin_state] = value
  end
  
  def vlan_name=(value)
@@ -50,7 +60,7 @@ Puppet::Type.type(:cnos_vlan).provide :vlan do
      params = {'vlan_name' => resource[:vlan_name], 
                'admin_state' => resource[:admin_state]}
      resp = Vlan.update_vlan(conn, resource[:vlan_id], params)
-   #@property_hash[:vlan_name] = value
+     @property_hash[:vlan_name] = value
  end
  
  def create
@@ -63,10 +73,10 @@ Puppet::Type.type(:cnos_vlan).provide :vlan do
  end
 
  def exists?
-     conn = Connect.new('./config.yml')
-     #@property_hash[:ensure] == :present
-     resp = Vlan.get_vlan_prop(conn, resource[:vlan_id])
-     resp != nil
+     #conn = Connect.new('./config.yml')
+     @property_hash[:ensure] == :present
+     #resp = Vlan.get_vlan_prop(conn, resource[:vlan_id])
+     #resp != nil
  end
 
  def destroy
